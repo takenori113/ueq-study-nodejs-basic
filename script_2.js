4ec25295e1d07dd65e34326058136bf06c2e0dd6
Ken Takahashi <takaken@redimpulz.com>
2023-11-28 10:48:23 +0900

    問題を追加

---
+++ script_2.js
@@ -0,0 +1,6 @@
+export const main = async () => {
+  const csvPath = "./seiseki.csv";
+  console.log("csvファイルの中身を出力");
+};
+
+main();
---
+++ script_3.js
@@ -0,0 +1,5 @@
+export const main = () => {
+  // sayHelloWorld()を呼ぶ
+};
+
+main();
